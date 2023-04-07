b170155cd1628494a1dc684764ed4dc3cd4624fb
maikeldiulio <maikeltecba@gmail.com>
2023-04-07 18:01:04 -0300

    finalização do projeto

---
+++ src/database/UserDatabase.ts
@@ -0,0 +1,31 @@
+import { UserDB } from "../types";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+    public static TABLE_USERS = "users"
+
+
+
+    public async insertUser(newUserDB: UserDB) {
+        await BaseDatabase
+            .connection(UserDatabase.TABLE_USERS)
+            .insert(newUserDB)
+    }
+
+    // public async findUserById(id: string) {
+    //     const [ userDB ]: UserDB[] | undefined[] = await BaseDatabase
+    //         .connection(UserDatabase.TABLE_USERS)
+    //         .where({ id })
+
+    //     return userDB
+    // }
+
+    public async findUserEmail(email: string): Promise<UserDB | undefined> {
+        const [userDB] = await BaseDatabase
+            .connection(UserDatabase.TABLE_USERS)
+            .select()
+            .where({ email })
+
+        return userDB
+    }
+}
